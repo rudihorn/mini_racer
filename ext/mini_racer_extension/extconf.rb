048da829d5a1ee02b59343c69f3804f1b2d45c18
Rudi Horn <rudi.horn@oracle.com>
2022-02-04 13:06:30 +0100

    move dependency

--- ext/mini_racer_extension/extconf.rb
+++ ext/mini_racer_extension/extconf.rb
@@ -1,9 +1,10 @@
+require 'mkmf'
+
 if RUBY_ENGINE == "truffleruby"
   File.write("Makefile", dummy_makefile($srcdir).join(""))
   return
 end
 
-require 'mkmf'
 require_relative '../../lib/mini_racer/version'
 gem 'libv8-node', MiniRacer::LIBV8_NODE_VERSION
 require 'libv8-node'
--- ext/mini_racer_loader/extconf.rb
+++ ext/mini_racer_loader/extconf.rb
@@ -1,9 +1,9 @@
+require 'mkmf'
+
 if RUBY_ENGINE == "truffleruby"
   File.write("Makefile", dummy_makefile($srcdir).join(""))
   return
 end
-
-require 'mkmf'
 
 extension_name = 'mini_racer_loader'
 dir_config extension_name
